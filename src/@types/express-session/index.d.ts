bed6c2abbe1738adea0bd76c06c04e438bd3c5dc
Adelmo <adelmo@connectlab.com>
2026-06-07 15:41:00 -0300

    cria AuthenticateUserService

---
+++ src/@types/express-session/index.d.ts
@@ -0,0 +1,17 @@
+// import "express-session";
+// // import { IUsuarioDTO } from '../../src/app/modules/usuario/dtos/IUsuarioDTO';
+
+// declare module "express-session" {
+//   export interface SessionData {
+//     loggedUsername: string;
+//     usuarioLogadoId: number;
+//     usuarioLogadoFoto: string;
+//     usuarioLogadoPermissoes: string;
+//     tokenUser: any;
+//     certificateMessage: string;
+//     certificateBadgeType: string;
+//     certificateIcon: string;
+//     subdomain: string;
+//     // loggedUser: IUsuarioDTO;
+//   }
+// }
---
+++ src/shared/infra/http/middlewares/EnsureAuthenticated.ts
@@ -0,0 +1,35 @@
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+
+import { auth } from "../../../../shared/config/auth";
+import { AppError } from "../../../../shared/errors/AppError";
+
+interface ITokenPayload {
+  sub: string;
+}
+
+export class EnsureAuthenticated {
+  async execute(request: Request, response: Response, next: NextFunction) {
+    const authHeader = request.headers.authorization;
+
+    if (!authHeader) {
+      throw new AppError("JWT token ausente", 401);
+    }
+
+    const [, token] = authHeader.split(" ");
+
+    try {
+      const decoded = verify(token, auth.jwt.secret);
+
+      const { sub } = decoded as ITokenPayload;
+
+      request.user = {
+        id: sub,
+      };
+
+      return next();
+    } catch {
+      throw new AppError("Invalid JWT token", 401);
+    }
+  }
+}
